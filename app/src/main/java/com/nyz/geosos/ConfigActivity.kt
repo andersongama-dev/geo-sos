075ebd8e20704c51e0acb54b4c97ed189f171eb8
Anderson Gama <andersongamasilva5@gmail.com>
2026-05-18 14:47:57 -0300

    feat: Saved preferences contact

--- app/src/main/java/com/nyz/geosos/ConfigActivity.kt
+++ app/src/main/java/com/nyz/geosos/ConfigActivity.kt
@@ -1,12 +1,23 @@
 package com.nyz.geosos
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ConfigActivity : AppCompatActivity() {
+
+    private lateinit var editTextContactName: EditText
+    private lateinit var editTextContactPhone: EditText
+    private lateinit var editTextContactMsg: EditText
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,47 @@ class ConfigActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // SharedPreferences
+        sharedPreferences = getSharedPreferences("socorro", MODE_PRIVATE)
+
+        // EditTexts
+        editTextContactName = findViewById(R.id.TextInputEditTextContactName)
+        editTextContactPhone = findViewById(R.id.TextInputEditTextContactPhone)
+        editTextContactMsg = findViewById(R.id.TextInputEditTextCustomMsg)
+
+        loadPreferences()
+
+        findViewById<Button>(R.id.buttonSave).setOnClickListener {
+            println("è muito massa")
+            savePreferences(
+                editTextContactName.toString(),
+                editTextContactPhone.toString(),
+                editTextContactMsg.toString()
+            )
+
+        }
+    }
+
+    private fun savePreferences(name: String, phone: String, msg: String) {
+        sharedPreferences.edit().putString("contactName", name)
+            .putString("contactPhone", phone)
+            .putString("contactMsg", msg)
+            .apply()
+
+        Toast.makeText(this, "Contato salvo com sucesso", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun clearPreferences() {
+        sharedPreferences.edit().clear().apply()
+
+        Toast.makeText(this, "Contanto excluido com sucesso", Toast.LENGTH_SHORT).show()
+        loadPreferences()
+    }
+
+    private fun loadPreferences() {
+        editTextContactName.setText(sharedPreferences.getString("contactName", ""))
+        editTextContactPhone.setText(sharedPreferences.getString("contactPhone", ""))
+        editTextContactMsg.setText(sharedPreferences.getString("contactMsg", ""))
     }
 }
